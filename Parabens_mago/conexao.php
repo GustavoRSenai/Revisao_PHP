eea553e4069fa9f511930960fded27127ccac4e3
Gustavo <gustavorsenai@gmail.com>
2025-03-17 15:08:40 -0300

    Initial commit

---
+++ Parabens_mago/conexao.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "Senai@118";
+    $dbname = "sistema";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    ?>
+</body>
+</html>
